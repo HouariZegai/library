787330503574d1ea2c8c36c88de29d33b6ea9000
HouariZegai <houarizegai14@gmail.com>
2022-06-16 18:23:08 +0100

    reafctor: remove run-env script

--- run-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DB_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}?reWriteBatchedInserts=true"
-#
-DB_OPTS="-Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD} -Dspring.datasource.url=${DB_URL}"
-#
-export JAVA_OPTIONS="${JAVA_OPTIONS:-} ${DB_OPTS}"
